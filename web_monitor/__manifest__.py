38dfd9cd64f5ede85e2cd08ec427fc47465299e3
durgeshgupt9 <durgeshgupt9@gmail.com>
2025-10-10 01:55:49 +0530

    Update __manifest__.py

--- web_monitor/__manifest__.py
+++ web_monitor/__manifest__.py
@@ -1,9 +1,9 @@
 {
     'name': 'Website Monitor',
-    'version': '1.0',
+    'version': '17.0.1.0.0',  # If your module is for Odoo 17.0
     'category': 'Tools',
     'summary': 'Monitor website URL and port, email alerts, manual check, status in header',
-    'author': 'Your Name',
+    'author': 'Odoo Community Association (OCA)',
     'depends': ['base', 'mail'],
     'data': [
         'security/ir.model.access.csv',
